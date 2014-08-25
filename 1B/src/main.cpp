d43f3fca6a5117295a26448649d7c5c9fb50b231
Andrew Montenigro <andrew.montenigro@gmail.com>
2014-08-25 16:51:58 -0500

    Added input files and functions from class
    added say.in and say.txt, tested opening say.in and worked perfectly.
    The executable file compiles in the 1B folder, not the src folder.

--- 1B/src/main.cpp
+++ 1B/src/main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 bool isVowel(char);
+bool containsVowel(string);
+
 
 int main() {
-	
+	fstream infile;
+	infile.open("say.in");
+	//infile.open("say.txt");
+	//Spec says say.in
+	string input = "";
+	infile >> input;
+	while(input != "end"){
+		cout << input << endl;
+		infile >> input;
+	}
+		
 	return 0;
-}
+}	//main
 
 bool isVowel(char c){
-	string test = "AEIOUaeiou";
-	if(test.find(c) == -1) return false;
-	else return true;
-}
+	string test = "AEIOUaeiou";				//contains both cases to make code reuse easy
+	if(test.find(c) == -1) return false;	//string::find() returns value -1 if char is NOT in string
+	return true;
+}	//isVowel
+
+bool containsVowel(string s){
+	for(int i = 0; i < s.length();i++){
+		if(isVowel(s[i])) return true;
+	}
+	return false;
+}	//containsVowel
