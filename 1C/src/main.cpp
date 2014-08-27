7fdbd455174a7f865542f5e0fd8aa9fc9f555511
Andrew Montenigro <andrew.montenigro@gmail.com>
2014-08-27 13:31:22 -0500

    Executable properly opens input file
    Executable compiled in src folder and was reading garbage data from
    infile stream. Fixed by moving input file into src folder.

--- 1C/src/main.cpp
+++ 1C/src/main.cpp
@@ -15,11 +15,19 @@ int main() {
 	ifstream infile;
 	ofstream outfile;
 	int loanDuration, depreciationRecords;
-	float downPayment, loanAmount, depreciationPercentage, currentValue, initialValue;
+	float downPayment, loanAmount, depreciationPercentage, currentValue, initialValue, aggregateDepreciation = 0.f;
 	
 	infile.open("loan.in");
 	outfile.open("loan.out");
-	
+	infile >> loanDuration;
+	infile >> downPayment;
+	infile >> loanAmount;
+	infile >> depreciationRecords;
+	cout << loanDuration << endl;
+	cout << downPayment << endl;
+	cout << loanAmount << endl;
+	cout << depreciationRecords << endl;
 	
 	return 0;
 }
+
