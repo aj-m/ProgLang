9607fc7acab8cef55e44574cebabae752b705b6d
Andrew Montenigro <andrew.montenigro@gmail.com>
2014-08-26 22:07:07 -0500

    1C Initial commit
    title

---
+++ 1C/src/main.cpp
@@ -0,0 +1,25 @@
+/*
+ *	Title: Loansome Car Buyer
+ *	Authors: Andrew Montenigro, Jessica Neal, Nate Richards
+ */
+
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+	ifstream infile;
+	ofstream outfile;
+	int loanDuration, depreciationRecords;
+	float downPayment, loanAmount, depreciationPercentage, currentValue, initialValue;
+	
+	infile.open("loan.in");
+	outfile.open("loan.out");
+	
+	
+	return 0;
+}
